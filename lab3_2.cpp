3167e587de94f159e52dbd76e39e8034562bce5f
Braxton Prendergast <bprendergast12@hotmail.com>
2025-09-04 08:42:50 -0700

    lab3.2 is completed

---
+++ lab3_2.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file Lab3_2
+ *
+ * This This program has one syntax error and one logic error.
+ *
+ * @author Braxton Prendergast
+ * @date   9/4/2025
+ */
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    double length = 0.0,  // Length of a room in feet
+           width = 0.0,   // Width of a room in feet
+           area = 0.0;        // Area of the room in sq. ft.
+
+    // Get the room dimensions
+    cout << "Enter room length (in feet): ";
+    cin  >> length;
+
+    cout << "Enter room width (in feet): ";
+    cin  >> width;
+
+    // Compute and display the area
+    area = length * width;
+    cout << "The area of the room is " << area 
+         << " square feet." << endl;
+
+    return 0;
+}
